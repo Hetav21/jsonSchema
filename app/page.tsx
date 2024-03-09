838b8cde08a511e829de1bd131620095d92465f1
Hetav21 <shahhetav2106@gmail.com>
2024-03-09 21:35:56 +0530

    init

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div>
+    <div> Hello World </div>
+    <div> Hello World </div>
+    <div> Hello World </div>
+    <div> Hello World </div>
+    <div> Hello World </div>
+    <div> Hello World </div>
+    </div>
+  );
+}
